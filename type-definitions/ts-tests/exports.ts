dde1ead7df5f17b29461543411c49c6b76c8404d
Nataly Shrits <nataly87s@gmail.com>
2018-10-12 21:41:03 +0300

    add type tests

---
+++ type-definitions/ts-tests/exports.ts
@@ -0,0 +1,16 @@
+import * as recomponent from 'react-recomponent';
+import { NoUpdate, Update, SideEffects, UpdateWithSideEffects, ReComponent, RePureComponent } from 'react-recomponent';
+
+NoUpdate; // $ExpectType () => NoUpdateAction
+Update; // $ExpectType <T>(state: T) => UpdateAction<T>
+SideEffects; // $ExpectType <T>(sideEffect: T) => SideEffectsAction<T>
+UpdateWithSideEffects; // $ExpectType <S, SE>(state: S, sideEffects: SideEffect<SE>) => UpdateWithSideEffectsAction<S, SE>
+ReComponent; // $ExpectType typeof ReComponent
+RePureComponent; // $ExpectType typeof RePureComponent
+
+recomponent.NoUpdate; // $ExpectType () => NoUpdateAction
+recomponent.Update; // $ExpectType <T>(state: T) => UpdateAction<T>
+recomponent.SideEffects; // $ExpectType <T>(sideEffect: T) => SideEffectsAction<T>
+recomponent.UpdateWithSideEffects; // $ExpectType <S, SE>(state: S, sideEffects: SideEffect<SE>) => UpdateWithSideEffectsAction<S, SE>
+recomponent.ReComponent; // $ExpectType typeof ReComponent
+recomponent.RePureComponent; // $ExpectType typeof RePureComponent
